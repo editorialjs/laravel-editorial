c544e6ff7bdf20026dfcd2f716c772b7bd4d3803
Jason Funk <jason@tough.space>
2020-08-26 16:01:19 +0300

    Get the ball rolling with a simple service provider.

---
+++ src/Providers/EditorialServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class EditorialServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
